c2c7cdd800a6c0f9a1e76c49c73fda617776ea4f
root <root@localhost.localdomain>
2019-03-17 12:10:49 +0000

    fix bug of performance script

--- test/count.sh
+++ test/count.sh
@@ -2,10 +2,12 @@
 
 
 while :;do
-        res=`docker-compose -f mysql.yml mysql_0 mysql -uroot -root test_0 -N -s -e "select count(1) from news;select count(1) from news_bak;" | tr -s 'A-Z' 'a-z' | grep -v "warn" | xargs`
+        res=`docker-compose -f mysql.yml exec mysql_0 mysql -uroot -proot test_0 -N -s -e "select count(1) from news;select count(1) from news_bak;" 2>/dev/null | tr -s 'A-Z' 'a-z' | grep -v "warn" | xargs`
         src=`echo $res | awk '{print $1}'`
         dst=`echo $res | awk '{print $1}'`
+	timestamp=$(date +"%Y%m%d-%H%M%S")
+	echo "ts=$timestamp"
         echo "src=$src"
         echo "dst=$dst"
-        sleep 5
+        sleep 1
 done
--- test/load.sh
+++ test/load.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-docker-compose -f mysql.yml mysql_0 mysql -uroot -root test_0 -N -s -e "delete from news;delete from news_bak;"
-for file in data/csvs/;do
+docker-compose -f mysql.yml exec mysql_0 mysql -uroot -proot test_0 -N -s -e "delete from news;delete from news_bak;"
+for file in data/csvs/*;do
+        echo $file
         cp $file data/news.csv
         docker-compose -f mysql.yml exec mysql_0 mysqlimport --fields-terminated-by=, --verbose --local -uroot -proot test_0 /Data/news.csv
 done
