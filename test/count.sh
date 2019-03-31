f053e1d384e92bf12b409c30e7c15a92fa3a5998
zzt <zengzt93@gmail.com>
2019-03-31 16:19:15 +0800

    fix wrong script.

--- test/count.sh
+++ test/count.sh
@@ -2,9 +2,10 @@
 
 
 while :;do
-        res=`docker-compose -f mysql.yml mysql_0 mysql -uroot -root test_0 -N -s -e "select count(1) from news;select count(1) from news_bak;" | tr -s 'A-Z' 'a-z' | grep -v "warn" | xargs`
-        src=`echo $res | awk '{print $1}'`
-        dst=`echo $res | awk '{print $1}'`
+        timestamp=$(date +"%Y%m%d-%H%M%S")
+        src=`docker-compose -f mysql.yml exec mysql_0 mysql -uroot -proot test_0 -N -s -e "select count(1) from news" | tr -s 'A-Z' 'a-z' | grep -v "warn" | xargs`
+        dst=`docker-compose -f mysql.yml exec mysql_0 mysql -uroot -psimu123 -h192.168.1.204 test_0 -N -s -e "select count(1) from news_bak" | tr -s 'A-Z' 'a-z' | grep -v "warn" | xargs`
+        echo "time: $timestamp"
         echo "src=$src"
         echo "dst=$dst"
         sleep 5
--- test/load.sh
+++ test/load.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose -f mysql.yml mysql_0 mysql -uroot -root test_0 -N -s -e "delete from news;delete from news_bak;"
+docker-compose -f mysql.yml exec mysql_0 mysql -uroot -proot test_0 -N -s -e "delete from news"
+docker-compose -f mysql.yml exec mysql_0 mysql -usimu -psimu123 test_0 -N -s -e "delete from news_bak;"
 for file in data/csvs/;do
         cp $file data/news.csv
         docker-compose -f mysql.yml exec mysql_0 mysqlimport --fields-terminated-by=, --verbose --local -uroot -proot test_0 /Data/news.csv
